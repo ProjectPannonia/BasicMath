ce5db98c0e15bbbda7c7fb7400fc25a542071207
Adam Letenyei <letenyeidam89@gmail.com>
2019-12-22 19:50:26 +0100

    Lotto class implemented

--- BasicMath/Program.cs
+++ BasicMath/Program.cs
@@ -2,6 +2,49 @@
 
 namespace BasicMath
 {
+    class Lotto
+    {
+        public int[] getLottoNumbers()
+        {
+            int[] res = new int[5];
+            
+            Random random = new Random();
+            
+            for(int i = 0; i < res.Length; i++)
+            {
+                int actual = random.Next(1, 91);
+                if (containsActualNumber(res, actual))
+                {
+                    --i;
+                    continue;
+                }
+                else
+                {
+                    res[i] = actual;
+                }
+            }
+            return res;
+        }
+        //Return true, if array contain zero
+        private bool containsZero(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0) return true;
+            }
+            return false;
+        }
+        // Return true, if actual number already in array
+        private bool containsActualNumber(int[] arr, int num)
+        {
+            bool flag = false;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num) flag = true;
+            }
+            return flag;
+        }
+    }
     class Factorial
     {
         public String getFactorial(int number)
@@ -55,12 +98,30 @@ namespace BasicMath
             Console.WriteLine(math.biggestDivisor(a, b));
             //math.biggestDivisor(a, b);
             */
-
+            /*
             Factorial factorial = new Factorial();
             Console.Write("Kérlek add meg a keresett számot: ");
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine(factorial.getFactorial(num));
-
+            */
+            /*int a = 5;
+            Console.WriteLine("PostFix: " + a++);
+            Console.WriteLine("PreFix: " + ++a);
+            */
+            Lotto lotto = new Lotto();
+            bool flag = false;
+            do
+            {
+                int[] myNumbers = lotto.getLottoNumbers();
+                for (int i = 0; i < myNumbers.Length; i++)
+                {
+                    Console.WriteLine(i + ". number: " + myNumbers[i]);
+                }
+                Console.WriteLine("Sorsoljak neked új számokat?(y/n)");
+                string answer = Console.ReadLine();
+                if (answer.Equals("y")) flag = true;
+                if (answer.Equals("n")) break;
+            } while (flag);
         }
     }
 }
